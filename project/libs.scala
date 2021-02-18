b0550fa91873a28af988a65398ef2d6193a0ea42
Scala Steward <me@scala-steward.org>
2021-02-18 20:46:42 +0100

    Update postgresql to 42.2.19

--- project/libs.scala
+++ project/libs.scala
@@ -19,7 +19,7 @@ object libs {
 
   // https://jdbc.postgresql.org/
   // BSD
-  val postgres = "org.postgresql" % "postgresql" % "42.2.18"
+  val postgres = "org.postgresql" % "postgresql" % "42.2.19"
 
   // https://github.com/MariaDB/mariadb-connector-j
   // LGPL-2.1
