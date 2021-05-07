36a92c39b974c318b264d648e785bc11f3959749
Scala Steward <me@scala-steward.org>
2021-05-07 06:14:47 +0200

    Update doobie-core to 0.13.2

--- project/libs.scala
+++ project/libs.scala
@@ -11,7 +11,7 @@ object libs {
 
   // https://github.com/tpolecat/doobie
   // MIT
-  val doobieCore = "org.tpolecat" %% "doobie-core" % "0.13.1"
+  val doobieCore = "org.tpolecat" %% "doobie-core" % "0.13.2"
 
   // https://github.com/scodec/scodec-bits
   // 3-clause BSD
