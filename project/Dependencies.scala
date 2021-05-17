eddaad2504ece6366120716325088ad4424983c0
Scala Steward <me@scala-steward.org>
2021-05-17 17:15:59 +0200

    Update scala-library to 2.13.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val organizeImports = "0.5.0"
     val postgres        = "42.2.20"
     val scala212        = "2.12.13"
-    val scala213        = "2.13.5"
+    val scala213        = "2.13.6"
     val scodec          = "1.1.27"
     val tika            = "1.26"
   }
